6b03285a5d944ad15701f6693e16ae49397f9d25
Rooh-care <ahmed_j51@yahoo.com>
2026-08-07 20:23:32 +0300

    Add products data for DABO cleansers

---
+++ products.js
@@ -0,0 +1,32 @@
+const products = [
+
+{
+nameAr:"غسول دابو بالفحم",
+nameEn:"DABO Charcoal Foam Cleanser",
+price:"12000",
+image:"images/charcoal.jpg.png",
+category:"تنظيف البشرة",
+description:"تنظيف عميق للبشرة وتقليل الدهون والشوائب"
+},
+
+
+{
+nameAr:"غسول دابو الحلزون الأسود",
+nameEn:"DABO Black Snail Foam Cleanser",
+price:"12000",
+image:"images/snail.jpg.png",
+category:"تنظيف البشرة",
+description:"تنظيف لطيف مع ترطيب وعناية للبشرة"
+},
+
+
+{
+nameAr:"غسول دابو بالهيالورونيك",
+nameEn:"DABO Hyaluronic Acid Foam Cleanser",
+price:"12000",
+image:"images/hyaluronic.jpg.png",
+category:"تنظيف البشرة",
+description:"تنظيف عميق مع دعم ترطيب البشرة"
+}
+
+];
