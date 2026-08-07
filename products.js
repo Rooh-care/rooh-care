c5c9bd1997873b4a88534eaf6a58b5ad3e19bc92
Rooh-care <ahmed_j51@yahoo.com>
2026-08-07 22:35:36 +0300

    Remove several product entries from products.js
    Removed multiple products from the products list in products.js.

--- products.js
+++ products.js
@@ -9,37 +9,6 @@ category:"تنظيف البشرة",
 description:"تنظيف عميق للبشرة وتقليل الدهون والشوائب"
 },
 
-
-{
-nameAr:"غسول دابو الحلزون الأسود",
-nameEn:"DABO Black Snail Foam Cleanser",
-price:"12000",
-image:"images/snail.jpg.png",
-category:"تنظيف البشرة",
-description:"تنظيف وترطيب لطيف للبشرة"
-},
-
-
-{
-nameAr:"غسول دابو الهيالورونيك",
-nameEn:"DABO Hyaluronic Acid Foam Cleanser",
-price:"12000",
-image:"images/hyaluronic.jpg.png",
-category:"تنظيف البشرة",
-description:"تنظيف عميق مع ترطيب البشرة"
-},
-
-
-{
-nameAr:"سيروم الكولاجين",
-nameEn:"USHAS COLLAGEN SERUM",
-price:"10000",
-image:"images/serum-collagen.jpg",
-category:"السيرومات",
-description:"تعزيز نضارة وشد البشرة، ترطيب عالي"
-},
-
-
 {
 nameAr:"سيروم فيتامين سي",
 nameEn:"USHAS VIT C",
