359e40f9879c640eb5c4bddea403f66314818af0
Rooh-care <ahmed_j51@yahoo.com>
2026-08-07 21:06:19 +0300

    Add product details for skincare cleansers

---
+++ product-details.js
@@ -0,0 +1,70 @@
+const productDetails = {
+
+charcoal:{
+
+nameAr:"غسول دابو بالفحم",
+
+nameEn:"DABO Charcoal Foam Cleanser",
+
+image:"images/charcoal.jpg.png",
+
+price:"12000 دينار",
+
+description:
+"تنظيف عميق للبشرة وتقليل الدهون والشوائب",
+
+benefits:[
+"تنظيف المسام بعمق",
+"تقليل الدهون الزائدة",
+"منح البشرة إحساساً بالانتعاش"
+]
+
+},
+
+
+
+snail:{
+
+nameAr:"غسول دابو الحلزون الأسود",
+
+nameEn:"DABO Black Snail Foam Cleanser",
+
+image:"images/snail.jpg.png",
+
+price:"12000 دينار",
+
+description:
+"تنظيف وترطيب لطيف للبشرة",
+
+benefits:[
+"ترطيب البشرة",
+"تهدئة البشرة",
+"المساعدة في تحسين مظهر البشرة"
+]
+
+},
+
+
+
+hyaluronic:{
+
+nameAr:"غسول دابو الهيالورونيك",
+
+nameEn:"DABO Hyaluronic Acid Foam Cleanser",
+
+image:"images/hyaluronic.jpg.png",
+
+price:"12000 دينار",
+
+description:
+"تنظيف عميق مع دعم ترطيب البشرة",
+
+benefits:[
+"ترطيب مكثف",
+"الحفاظ على نعومة البشرة",
+"مناسب للاستخدام اليومي"
+]
+
+}
+
+};
