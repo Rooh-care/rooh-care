248286f469e4f7e1a86cf35f0325dc57a67bd6d1
Rooh-care <ahmed_j51@yahoo.com>
2026-08-07 20:35:51 +0300

    Update product display format and language

--- products.js
+++ products.js
@@ -1,62 +1,45 @@
 const container = document.getElementById("products-container");
 
-
-if(container){
+if (container && typeof products !== "undefined") {
 
 products.forEach(product => {
-
 
 container.innerHTML += `
 
 <div class="card">
 
-
 <img src="${product.image}" alt="${product.nameEn}">
 
 
 <h2>
-
 ${product.nameAr}
-
 <br>
-
 ${product.nameEn}
-
 </h2>
 
 
 <div class="rating">
-
 ⭐⭐⭐⭐⭐
-
 </div>
 
 
 <p class="available">
-
 متوفر الآن ✅
-
 </p>
 
 
 <p>
-
 ${product.description}
-
 </p>
 
 
 <div class="price">
-
-${product.price} IQD
-
+${product.price} دينار
 </div>
 
 
 <a class="order"
-
 href="https://wa.me/9647706136665?text=السلام عليكم ROOH CARE%0Aأرغب بطلب ${product.nameAr}"
-
 target="_blank">
 
 🟢 اطلب الآن
@@ -69,6 +52,5 @@ target="_blank">
 `;
 
 });
-
 
 }
