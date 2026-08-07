afc9e515eee5df9d35fe0f17c14e7246aae794fb
Rooh-care <ahmed_j51@yahoo.com>
2026-08-08 02:02:32 +0300

    Fix duplicate product entries in products.js

--- products.js
+++ products.js
@@ -1,52 +1,48 @@
 const products = [
 
 {
-nameAr:"غسول دابو بالفحم",
-nameEn:"DABO Charcoal Foam Cleanser",
-price:"12000",
-image:"images/charcoal.jpg.png",
-category:"تنظيف البشرة",
-description:"تنظيف عميق للبشرة وتقليل الدهون والشوائب"
+nameAr: "غسول دابو بالفحم",
+nameEn: "DABO Charcoal Foam Cleanser",
+price: "12000",
+image: "images/charcoal.jpg.png",
+category: "تنظيف البشرة",
+description: "تنظيف عميق للبشرة وتقليل الدهون والشوائب"
 },
 
-
 {
-nameAr:"غسول دابو الحلزون الأسود",
-nameEn:"DABO Black Snail Foam Cleanser",
-price:"12000",
-image:"images/snail.jpg.png",
-category:"تنظيف البشرة",
-description:"تنظيف وترطيب لطيف للبشرة"
+nameAr: "غسول دابو الحلزون الأسود",
+nameEn: "DABO Black Snail Foam Cleanser",
+price: "12000",
+image: "images/snail.jpg.png",
+category: "تنظيف البشرة",
+description: "تنظيف وترطيب لطيف للبشرة"
 },
 
-
 {
-nameAr:"غسول دابو الهيالورونيك",
-nameEn:"DABO Hyaluronic Acid Foam Cleanser",
-price:"12000",
-image:"images/hyaluronic.jpg.png",
-category:"تنظيف البشرة",
-description:"تنظيف عميق مع ترطيب البشرة"
+nameAr: "غسول دابو الهيالورونيك",
+nameEn: "DABO Hyaluronic Acid Foam Cleanser",
+price: "12000",
+image: "images/hyaluronic.jpg.png",
+category: "تنظيف البشرة",
+description: "تنظيف عميق مع ترطيب البشرة"
 },
 
-
 {
-nameAr:"سيروم الكولاجين",
-nameEn:"USHAS COLLAGEN SERUM",
-price:"10000",
-image:"images/serum-collagen.jpg",
-category:"السيرومات",
-description:"تعزيز نضارة وشد البشرة، ترطيب عالي"
+nameAr: "سيروم الكولاجين",
+nameEn: "USHAS COLLAGEN SERUM",
+price: "10000",
+image: "images/serum-collagen.jpg",
+category: "السيرومات",
+description: "تعزيز نضارة وشد البشرة، ترطيب عالي"
 },
 
-  
 {
-nameAr:"سيروم فيتامين سي",
-nameEn:"USHAS VIT C",
-price:"10000",
-image:"images/vitc-serum.jpg",
-category:"السيرومات",
-description:"تغذية البشرة، محاربة أكسدة البشرة، ترطيب عالي، نضارة للبشرة"
+nameAr: "سيروم فيتامين سي",
+nameEn: "USHAS VIT C",
+price: "10000",
+image: "images/vitc-serum.jpg",
+category: "السيرومات",
+description: "تغذية البشرة، محاربة أكسدة البشرة، ترطيب عالي، نضارة للبشرة"
 }
 
 ];
